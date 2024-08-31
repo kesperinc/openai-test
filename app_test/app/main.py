0e898588d2dc04c8fe6386ee4b7dd0482bb2f9b5
Sungho Kim <kesperinc@msn.com>
2024-08-31 20:31:23 +0900

    First App structure setup

--- app_test/app/main.py
+++ app_test/app/main.py
@@ -6,7 +6,7 @@ import os
 app = FastAPI()
 
 # 정적 파일을 /static 경로로 서빙합니다.
-app.mount("templates", StaticFiles(directory="templates"), name="static")
+#app.mount("templates", StaticFiles(directory="static"), name="static")
 
 # / 경로에서 index.html을 제공
 @app.get("/", response_class=HTMLResponse)
