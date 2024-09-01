05f847781e446ea8ba30ece89b1d92e9aeb26a52
Sung Ho Kim <kesperinc@msn.com>
2024-09-01 19:28:00 +0900

    TexProcessing From Web

--- app_test/app/main.py
+++ app_test/app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles 
+from app.api.v1 import endpoints
+
 import os
+
 
 app = FastAPI()
 
 # 정적 파일을 /static 경로로 서빙합니다.
 #app.mount("templates", StaticFiles(directory="static"), name="static")
+
+# 라우터 등록
+app.include_router(endpoints.router, prefix="/api/v1")
 
 # / 경로에서 index.html을 제공
 @app.get("/", response_class=HTMLResponse)
